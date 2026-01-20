03cd11d93c40801aa68b42120a9b18cb0bdb9552
Morgan Harrison <mrgnharrisn@gmail.com>
2026-01-21 11:19:07 +1300

    Changes?

--- premake5.lua
+++ premake5.lua
@@ -10,12 +10,7 @@ project "GLFW"
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+		"src/**.c"
 	}
 	filter "system:linux"
 		pic "On"
